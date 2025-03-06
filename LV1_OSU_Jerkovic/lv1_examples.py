f6f0b9ee7d014cd72f9b75304d13261c48788f60
Ana Jerković <anajerkovic3@gmail.com>
2025-03-06 12:00:12 +0100

    Add files via upload

---
+++ LV1_OSU_Jerkovic/lv1_examples.py
@@ -0,0 +1,16 @@
+x = 23
+print ( x )
+x = x + 7
+print ( x ) # komentar : ispis varijable na ekranu
+
+
+x = 23
+y = x > 10
+print ( y )
+
+
+x = 23
+if x < 10:
+    print (" x je manji od 10 ")
+else :
+    print (" x je veci ili jednak od 10 ")
