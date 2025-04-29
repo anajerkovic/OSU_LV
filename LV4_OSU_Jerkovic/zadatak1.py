e83c53161f2907e30aabe0a38894fda8a0704651
ana.jerkovic <anajerkovic3@gmail.com>
2025-04-29 18:21:43 +0200

    Fix and finish LV4 exercises

--- LV4_OSU_Jerkovic/zadatak1.py
+++ LV4_OSU_Jerkovic/zadatak1.py
@@ -1,3 +1,11 @@
+'''
+Skripta zadatak_1.py ucitava podatkovni skup iz data_C02_emission.csv.
+Potrebno je izgraditi i vrednovati model koji procjenjuje emisiju C02 plinova na temelju ostalih numerickih ulaznih veličina. 
+Detalje oko ovog podatkovnog skupa mogu se pronaći u 3. 
+laboratorijskoj vježbi.
+'''
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,79 +16,89 @@ import sklearn.linear_model as lm
 import sklearn.metrics as sk
 
 data = pd.read_csv('data_C02_emission.csv')
-
+data.info()
 
 '''
 Odaberite željene numericke veličine specificiranjem liste s nazivima stupaca. Podijelite
 podatke na skup za ucenje i skup za testiranje u omjeru 80%-20%.
 '''
-input_var = ['Engine Size (L)', 'Cylinders', 'Fuel Consumption City (L/100km)', 'Fuel Consumption Hwy (L/100km)', 'Fuel Consumption Comb (L/100km)', 'Fuel Consumption Comb (mpg)']
+input_var = ['Engine Size (L)', 'Cylinders', 'Fuel Consumption City (L/100km)', 'Fuel Consumption Hwy (L/100km)', 
+             'Fuel Consumption Comb (L/100km)', 'Fuel Consumption Comb (mpg)']
 output = ['CO2 Emissions (g/km)']
-
 X = data[input_var]
 y = data[output]
 
+# podjela skupa na skup za ucenje i skup za testiranje 80/20
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state =1 )
 
 
 
 '''
 Pomocu matplotlib biblioteke i dijagrama raspršenja prikažite ovisnost emisije C02 plinova 
-o jednoj numerickoj veličini. Pri tome podatke koji pripadaju skupu za učenje označite ˇ
+o jednoj numerickoj veličini(Engine Size). Pri tome podatke koji pripadaju skupu za učenje označite 
 plavom bojom, a podatke koji pripadaju skupu za testiranje oznacite crvenom bojom.
 '''
-plt.scatter(X_train['Engine Size (L)'], y_train, c ='Red')
-plt.scatter(X_test['Engine Size (L)'], y_test, c='Blue')
+plt.scatter(X_train['Engine Size (L)'], y_train, c ='Blue', label='Training data')
+plt.scatter(X_test['Engine Size (L)'], y_test, c='Red', label='Testing data')
 plt.xlabel('Engine Size')
 plt.ylabel('CO2 Emissions')
+plt.legend()
 plt.show()
 
 
 
 '''
-Izvršite standardizaciju ulaznih velicina skupa za učenje. Prikažite histogram vrijednosti 
-jedne ulazne velicine prije i nakon skaliranja. Na temelju dobivenih parametara skaliranja 
-transformirajte ulazne velicine skupa podataka za testiranje.
+Izvršite standardizaciju ulaznih velicina skupa za učenje. 
+Prikažite histogram vrijednosti jedne ulazne velicine(Fuel Consumption City (L/100km)) prije i nakon skaliranja. 
+Na temelju dobivenih parametara skaliranja transformirajte ulazne velicine skupa podataka za testiranje.
 '''
+plt.subplot(1,2,1)
 plt.hist(X_train['Fuel Consumption City (L/100km)'])
-plt.show()
+plt.title("Prije Standardizacije")
+plt.tight_layout()
 
-sc = MinMaxScaler ()
-X_train_n = sc.fit_transform( X_train )
+scaler = MinMaxScaler()
+X_train_n = scaler.fit_transform( X_train )
+
+plt.subplot(1,2,2)
 plt.hist(X_train_n[:,0])
+plt.title("Poslije Standardizacije")
+plt.tight_layout()
 plt.show()
 
-X_test_n = sc.transform( X_test )
+X_test_n = scaler.transform( X_test )
 
 
 
 '''
-Izgradite linearni regresijski modeli. Ispišite u terminal dobivene parametre modela i
+Izgradite linearni regresijski modeli. 
+Ispišite u terminal dobivene parametre modela i
 povežite ih s izrazom 4.6.
 '''
 linearModel = lm.LinearRegression ()
-linearModel.fit( X_train_n , y_train )
+linearModel.fit( X_train_n , y_train ) #procjena parametara na temelju skupa za učenje/treniranje
 
 print(f"Parametri modela: {linearModel.coef_}")
 
 
 
 '''
-Izvršite procjenu izlazne velicine na temelju ulaznih veličina skupa za testiranje. Prikažite
-pomocu dijagrama raspršenja odnos između stvarnih vrijednosti izlazne veličine i procjene 
+Izvršite procjenu izlazne velicine na temelju ulaznih veličina skupa za testiranje. 
+Prikažite pomocu dijagrama raspršenja odnos između stvarnih vrijednosti izlazne veličine i procjene 
 dobivene modelom
 '''
-y_test_p = linearModel.predict ( X_test_n )
+y_test_p = linearModel.predict ( X_test_n ) # predikcija izlazne veličine na skupu podataka za testiranje
 plt.scatter(y_test, y_test_p)
+plt.title("Odnos između stvarnih vrijednosti \n izlazne veličine i procjene dobivene modelom")
 plt.xlabel("Stvarne vrijednosti")
-plt.ylabel("Predvidene vrijednosti")
+plt.ylabel("Procjenjene vrijednosti")
 plt.show()
 
 
 
 '''
 Izvršite vrednovanje modela na nacin da izračunate vrijednosti regresijskih metrika na 
-skupu podataka za testiranje.
+skupu podataka za testiranje.Sve funkcije su dio scikit-learn biblioteke
 '''
 MAE = sk.mean_absolute_error( y_test , y_test_p )
 MSE = sk.mean_squared_error( y_test , y_test_p )
--- LV4_OSU_Jerkovic/zadatak2.py
+++ LV4_OSU_Jerkovic/zadatak2.py
@@ -3,8 +3,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import sklearn.linear_model as lm
 import sklearn.metrics as sk
 
 data = pd.read_csv('data_C02_emission.csv')
+data.info()
+'''
+Na temelju rješenja prethodnog zadatka izradite model koji koristi i kategoricku 
+varijable „Fuel Type“ kao ulaznu velicinu. Pri tome koristite 1-od-K kodiranje kategoričkih 
+velicina. 
+Radi jednostavnosti nemojte skalirati ulazne veličine - no minmax. Komentirajte dobivene rezultate. 
+Kolika je maksimalna pogreška u procjeni emisije C02 plinova u g/km? O kojem se modelu
+vozila radi?
+'''
+
+#ovaj dio je isti kao i u 1. zadatku
+input_var = ['Engine Size (L)', 
+             'Cylinders', 
+             'Fuel Type', 
+             'Fuel Consumption City (L/100km)',
+             'Fuel Consumption Hwy (L/100km)',
+             'Fuel Consumption Comb (L/100km)',
+             'Fuel Consumption Comb (mpg)',
+             ]
+output_var = ['CO2 Emissions (g/km)']
+X = data[input_var]
+y = data[output_var]
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=1)
+
+
+#Kodiranje nominalne kategoricke velicine Fuel Type - one hot encoding
+ohe = OneHotEncoder()
+X_encoded_train = ohe.fit_transform(X_train[['Fuel Type']]).toarray()
+X_encoded_test = ohe.fit_transform(X_test[['Fuel Type']]).toarray()
+
+# izgrađivanje linearnog regresijskog modela
+linearModel = lm.LinearRegression()
+linearModel.fit(X_encoded_train, y_train)
+y_test_p = linearModel.predict(X_encoded_test)
+
+#izračun maksimalne pogreške ME
+ME = sk.max_error(y_test, y_test_p)
+print(f"Max Error: {ME}") # 331.45
+
+
+error = np.abs(y_test_p, y_test)
+max_error_id = np.argmax(error)
+
+max_error_model = data.iloc[max_error_id, 1]
+print(max_error_model)
